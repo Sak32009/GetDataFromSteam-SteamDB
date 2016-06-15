3e8396bcede8aacb4d2147fe433ae0b4c32b1ccc
Sak32009 <ak32009@users.noreply.github.com>
2016-06-15 21:57:30 +0200

    Test

--- sak32009-get-dlc-info-from-steamdb.user.js
+++ sak32009-get-dlc-info-from-steamdb.user.js
@@ -7,8 +7,8 @@
 // @license          MIT
 // @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB
 // @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
-// @updateURL        https://cdn.rawgit.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb.meta.js
-// @downloadURL      https://cdn.rawgit.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb.user.js
+// @updateURL        https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb.meta.js
+// @downloadURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb.user.js
 // @icon             https://steamdb.info/static/logos/favicon-32x32.png
 // @include          http://steamdb.info/app/*
 // @include          https://steamdb.info/app/*
