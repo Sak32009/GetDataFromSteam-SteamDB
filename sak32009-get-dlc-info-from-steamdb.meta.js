35c26f43be2315b5b1db97797feca2a0b7c46b56
Sak32009 <ak32009@users.noreply.github.com>
2016-09-04 14:11:27 +0200

    Update

--- sak32009-get-dlc-info-from-steamdb.meta.js
+++ sak32009-get-dlc-info-from-steamdb.meta.js
@@ -4,7 +4,7 @@
 // @description      Get DLC Info from SteamDB.
 // @author           Sak32009
 // @contributor      CS.RIN.RU Users
-// @version          3.0.0
+// @version          3.0.1
 // @license          MIT
 // @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB
 // @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
