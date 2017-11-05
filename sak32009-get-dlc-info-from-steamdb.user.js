e4c028ab09bbcf4688eccbb19d7af004c3d7eb31
Sak32009 <Sak32009@users.noreply.github.com>
2017-11-05 12:40:53 +0200

    Update 3.4.5

--- sak32009-get-dlc-info-from-steamdb.user.js
+++ sak32009-get-dlc-info-from-steamdb.user.js
@@ -4,7 +4,7 @@
 // @description      Get DLC Info from SteamDB.
 // @author           Sak32009
 // @contributor      CS.RIN.RU Users
-// @version          3.4.4
+// @version          3.4.5
 // @license          MIT
 // @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/
 // @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
@@ -72,7 +72,7 @@
         formats: {
             // CREAMAPI
             creamAPI: {
-                name: "CREAMAPI v3.0.0.3 Hotfix",
+                name: "CREAMAPI v3.1.0.0",
                 ini: {
                     name: "cream_api.ini",
                     data: `[steam]
@@ -94,15 +94,26 @@ orgapi64 = steam_api64_o.dll
 ; Enable/disable extra protection bypasser.
 ; Default is "false".
 extraprotection = false
-; This option will force the usage of the default Steam user data folder.
-; Default is "true".
-;forceuserdatafolder = false
 ; The game will think that you're offline (supported by some games).
 ; Default is "false".
 forceoffline = false
 ; Some games are checking for the low violence presence.
 ; Default is "false".
 ;lowviolence = true
+; Installation path for the game.
+; Note, that you can use .\\ to set the parent directory (from where executable file is located).
+; Maximum number of parent directories: 5 (.\\.\\.\\.\\.\\)
+; Default is the path to current working directory.
+;installdir = .\\
+; Use DLC id as the appended installation directory.
+; e.g. <install_directory>\\480
+; Default is "true".
+;dlcasinstalldir = false
+; Turn on the wrapper mode.
+; Default is "false".
+wrappermode = false
+
+[steam_misc]
 ; Disables the internal SteamUser interface handler.
 ; Does have an effect on the games that are using the license check for the DLC/application.
 ; Default is "false".
@@ -111,9 +122,18 @@ disableuserinterface = false
 ; Does have an effect on the games that are checking for the actual AppId (only matters when "wrappermode" is set to "true").
 ; Default is "false".
 disableutilsinterface = false
-; Turn on the wrapper mode.
+; This option will force the usage of the default Steam user data folder.
+; Default is "true".
+;forceuserdatafolder = false
+; Unlock/Lock Steam parental restrictions.
+; Default is "true".
+;unlockparentalrestrictions = false
+; SteamId to override. Note that this action could be risky !
+; This option can only work if "disableuserinterface = false".
+;steamid = 0
+; Bypass VAC signature check. Note that this action could be risky !
 ; Default is "false".
-wrappermode = false
+;signaturebypass = true
 
 [steam_wrapper]
 ; Application ID to override (used when the wrapper mode is on)
@@ -138,103 +158,10 @@ saveindirectory = false
 ; e.g. : 247295 = Saints Row IV - GAT V Pack
 ; If the DLC is not specified in this section
 ; then it won't be unlocked
-[dlcEach]{dlc_id} = {dlc_name}\n[/dlcEach]`
-                },
-                options: {}
-            },
-
-            // CREAMAPI v2.0.0.7
-            creamAPI_2_0_0_7: {
-                name: "CREAMAPI v2.0.0.7",
-                ini: {
-                    name: "cream_api.ini",
-                    data: `[steam]
-; Application ID (http://store.steampowered.com/app/%appid%/)
-appid = [steamdb]appID[/steamdb]
-; Force the usage of specific language.
-; Uncomment this option to turn it on.
-;language = german
-; Enable/disable automatic DLC unlock. Default option is set to "false".
-; Keep in mind that this option is highly experimental and won't
-; work if game wants to call each DLC by index.
-unlockall = false
-; Original Valve's steam_api.dll.
-; Default is "steam_api_o.dll".
-orgapi = steam_api_o.dll
-; Original Valve's steam_api64.dll.
-; Default is "steam_api64_o.dll".
-orgapi64 = steam_api64_o.dll
-; Enable/disable extra protection bypasser.
-; Default is "false".
-extraprotection = false
-; ExtraProtection level.
-; Default is "0".
-; Available options :
-; 0 = minimum, 1 = medium, 2 = maximum
-extraprotectionlevel = 0
-; Turn on the "light" wrapper mode.
-; Default is "false".
-wrappermode = false
-; Enable/disable logging of the DLC functions.
-; Default is "false".
-; If you use log_build, uncomment this option to turn it on.
-;log = false
-
-[steam_wrapper]
-; Application ID to override (used when the wrapper mode is on)
-newappid = 0
-; Load steam emulator library.
-; Default is "false".
-loademu = false
-; Emulator library that is used for the stats
-; and storage handling.
-; Default is "emu.dll".
-emudll = emu.dll
-; Use the emulator storage system.
-; Default is "false".
-wrapperremotestorage = false
-; Use the emulator stats/achievements system.
-; Default is "false".
-wrapperuserstats = false
-; Use the emulator utils system.
-; Default is "false".
-wrapperutils = false
-; User the emulator callbacks system.
-; Default is "false".
-wrappercallbacks = false
-
-[dlc_subscription]
-; This will check if the specifed
-; DLC is owned by the user.
-; Format: <dlc_id> = <true/false>
-; e.g. : 12345 = true
-;        12346 = true
-;        12347 = true
-; If the DLC is not specified in this section
-; then it won't be subscribed.
-; Also if the value is set to "false" the DLC
-; won't be subscribed either.
-[dlcEach]{dlc_id} = true\n[/dlcEach]
-[dlc_index]
-; DLC handling.
-; Format: <dlc_index> = <dlc_id>
-; e.g. : 0 = 12345
-;        1 = 12346
-;        2 = 12347
-[dlcEach]{dlc_index} = {dlc_id}\n[/dlcEach]
-[dlc_names]
-; Names for the DLCs index put above.
-; Use this only if needed.
-; Format: <dlc_index> = <dlc_name>
-; e.g. : 0 = DLC Name 0
-;        1 = DLC Name 1
-;        2 = DLC Name 2
-[dlcEach]{dlc_index} = {dlc_name}\n[/dlcEach]
-[dlc_timestamp]
-; Specifies a unique unix timestamp for the purchased DLC (http://www.onlineconversion.com/unix_time.htm).
-; By default returns the current date timestamp (if nothing was specified).
-; Format: <dlc_id> = <timestamp>
-; e.g. : 12345 = 1420070400\n`
+[dlcEach]{dlc_id} = {dlc_name}\n[/dlcEach]
+[dlc_installdirs]
+; Installation path for the specific DLC (dependent from "installdir" option).
+; e.g. : 556760 = DLCRoot0`
                 },
                 options: {}
             },
@@ -244,10 +171,10 @@ wrappercallbacks = false
                 name: "GreenLuma [BATCH MODE]",
                 ini: {},
                 options: {},
-                callback(data, app) {
+                callback({info}, app) {
 
                     // BATCH
-                    let batch = data.info.replace(/; /g, ":: ");
+                    let batch = info.replace(/; /g, ":: ");
                     batch += `@echo off
 TITLE ${app.steamDB.appIDName} - ${app.info.name} by ${app.info.author} v${app.info.version}
 CLS
