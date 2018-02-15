b2b474052463d944b3e89ddc175f83e0f0ea4c18
Sak32009 <Sak32009@users.noreply.github.com>
2018-02-15 23:33:45 +0200

    Update 3.5.4

--- sak32009-get-dlc-info-from-steamdb.user.js
+++ sak32009-get-dlc-info-from-steamdb.user.js
@@ -4,7 +4,7 @@
 // @description      Get DLC Info from SteamDB.
 // @author           Sak32009
 // @contributor      CS.RIN.RU Users
-// @version          3.5.3
+// @version          3.5.4
 // @license          MIT
 // @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/
 // @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
@@ -72,7 +72,7 @@
         formats: {
             // CREAMAPI
             creamAPI: {
-                name: "CREAMAPI v3.4.0.0",
+                name: "CREAMAPI v3.4.1.0",
                 callback({info}, app){
                     return {
                         name: "cream_api.ini",
@@ -127,6 +127,9 @@ disableuserinterface = false
 ; Does have an effect on the games that are checking for the actual AppId (only matters when "wrappermode" is set to "true").
 ; Default is "false".
 disableutilsinterface = false
+; Disable the internal reserve hook of the "Steam_RegisterInterfaceFuncs" function.
+; Default is "false".
+disableregisterinterfacefuncs = false
 ; Unlock/Lock Steam parental restrictions.
 ; Default is "true".
 ;unlockparentalrestrictions = false
@@ -156,9 +159,6 @@ saveindirectory = false
 ; Force the usage of a full save path instead of the relative one.
 ; Default is "false".
 forcefullsavepath = false
-; Type of the used internal callbacks system. Could be "0" or "1".
-; Default is "0".
-callbackstype = 0
 ; Disable internal callbacks system.
 ; Default is "false".
 ;disablecallbacks = true
@@ -189,6 +189,179 @@ achievementscount = 0
 ; Format: <dlc_id> = <true/false>
 ; e.g. : 812713531 = true
 ; Please refer to __README_WORKSHOP_EN__.txt for more details.`
+                    };
+                },
+                options: {}
+            },
+
+			// CREAMAPI v3.0.0.3 Hotfix
+            creamAPI_3_0_0_3_h: {
+                name: "CREAMAPI v3.0.0.3 Hotfix",
+                callback({info}, app){
+                    return {
+                        name: "cream_api.ini",
+                        data: `[steam]
+; Application ID (http://store.steampowered.com/app/%appid%/)
+appid = [steamdb]appID[/steamdb]
+; Force the usage of specific language.
+; Uncomment this option to turn it on.
+;language = german
+; Enable/disable automatic DLC unlock. Default option is set to "false".
+; Keep in mind that this option is highly experimental and won't
+; work if the game wants to call each DLC by index.
+unlockall = false
+; Original Valve's steam_api.dll.
+; Default is "steam_api_o.dll".
+orgapi = steam_api_o.dll
+; Original Valve's steam_api64.dll.
+; Default is "steam_api64_o.dll".
+orgapi64 = steam_api64_o.dll
+; Enable/disable extra protection bypasser.
+; Default is "false".
+extraprotection = false
+; This option will force the usage of the default Steam user data folder.
+; Default is "true".
+;forceuserdatafolder = false
+; The game will think that you're offline (supported by some games).
+; Default is "false".
+forceoffline = false
+; Some games are checking for the low violence presence.
+; Default is "false".
+;lowviolence = true
+; Disables the internal SteamUser interface handler.
+; Does have an effect on the games that are using the license check for the DLC/application.
+; Default is "false".
+disableuserinterface = false
+; Disables the internal SteamUtils interface handler.
+; Does have an effect on the games that are checking for the actual AppId (only matters when "wrappermode" is set to "true").
+; Default is "false".
+disableutilsinterface = false
+; Turn on the wrapper mode.
+; Default is "false".
+wrappermode = false
+
+[steam_wrapper]
+; Application ID to override (used when the wrapper mode is on)
+newappid = 0
+; Use the internal storage system.
+; Default is "false".
+wrapperremotestorage = false
+; Use the internal stats/achievements system.
+; Default is "false".
+wrapperuserstats = false
+; Store the data in the current directory (incl. stats)
+; By default the data will is stored at: %appdata%/CreamAPI/%appid%/
+; Default is "false".
+saveindirectory = false
+; Disable/Enable a StoreStats callback. Takes effect only if "wrapperuserstats" is set to "true".
+; Default is "true"
+;storestatscallback = false
+
+[dlc]
+; DLC handling.
+; Format: <dlc_id> = <dlc_description>
+; e.g. : 247295 = Saints Row IV - GAT V Pack
+; If the DLC is not specified in this section
+; then it won't be unlocked
+[dlcs]{dlc_id} = {dlc_name}\n[/dlcs]`
+                    };
+                },
+                options: {}
+            },
+
+			// CREAMAPI v2.0.0.7
+            creamAPI_2_0_0_7: {
+                name: "CREAMAPI v2.0.0.7",
+                callback({info}, app){
+                    return {
+                        name: "cream_api.ini",
+                        data: `[steam]
+; Application ID (http://store.steampowered.com/app/%appid%/)
+appid = [steamdb]appID[/steamdb]
+; Force the usage of specific language.
+; Uncomment this option to turn it on.
+;language = german
+; Enable/disable automatic DLC unlock. Default option is set to "false".
+; Keep in mind that this option is highly experimental and won't
+; work if game wants to call each DLC by index.
+unlockall = false
+; Original Valve's steam_api.dll.
+; Default is "steam_api_o.dll".
+orgapi = steam_api_o.dll
+; Original Valve's steam_api64.dll.
+; Default is "steam_api64_o.dll".
+orgapi64 = steam_api64_o.dll
+; Enable/disable extra protection bypasser.
+; Default is "false".
+extraprotection = false
+; ExtraProtection level.
+; Default is "0".
+; Available options :
+; 0 = minimum, 1 = medium, 2 = maximum
+extraprotectionlevel = 0
+; Turn on the "light" wrapper mode.
+; Default is "false".
+wrappermode = false
+; Enable/disable logging of the DLC functions.
+; Default is "false".
+; If you use log_build, uncomment this option to turn it on.
+;log = false
+
+[steam_wrapper]
+; Application ID to override (used when the wrapper mode is on)
+newappid = 0
+; Load steam emulator library.
+; Default is "false".
+loademu = false
+; Emulator library that is used for the stats
+; and storage handling.
+; Default is "emu.dll".
+emudll = emu.dll
+; Use the emulator storage system.
+; Default is "false".
+wrapperremotestorage = false
+; Use the emulator stats/achievements system.
+; Default is "false".
+wrapperuserstats = false
+; Use the emulator utils system.
+; Default is "false".
+wrapperutils = false
+; User the emulator callbacks system.
+; Default is "false".
+wrappercallbacks = false
+
+[dlc_subscription]
+; This will check if the specifed
+; DLC is owned by the user.
+; Format: <dlc_id> = <true/false>
+; e.g. : 12345 = true
+;        12346 = true
+;        12347 = true
+; If the DLC is not specified in this section
+; then it won't be subscribed.
+; Also if the value is set to "false" the DLC
+; won't be subscribed either.
+[dlcs]{dlc_id} = true\n[/dlcs]
+[dlc_index]
+; DLC handling.
+; Format: <dlc_index> = <dlc_id>
+; e.g. : 0 = 12345
+;        1 = 12346
+;        2 = 12347
+[dlcs]{dlc_index} = {dlc_id}\n[/dlcs]
+[dlc_names]
+; Names for the DLCs index put above.
+; Use this only if needed.
+; Format: <dlc_index> = <dlc_name>
+; e.g. : 0 = DLC Name 0
+;        1 = DLC Name 1
+;        2 = DLC Name 2
+[dlcs]{dlc_index} = {dlc_name}\n[/dlcs]
+[dlc_timestamp]
+; Specifies a unique unix timestamp for the purchased DLC (http://www.onlineconversion.com/unix_time.htm).
+; By default returns the current date timestamp (if nothing was specified).
+; Format: <dlc_id> = <timestamp>
+; e.g. : 12345 = 1420070400`
                     };
                 },
                 options: {}
