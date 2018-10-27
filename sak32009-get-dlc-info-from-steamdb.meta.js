f77852ed7ad087979cb4cb10f774db3b13a3f232
Sak32009 <Sak32009@users.noreply.github.com>
2018-10-27 20:48:09 +0200

    Update sak32009-get-dlc-info-from-steamdb.meta.js

--- sak32009-get-dlc-info-from-steamdb.meta.js
+++ sak32009-get-dlc-info-from-steamdb.meta.js
@@ -17,7 +17,6 @@
 // @require          https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require          https://raw.githubusercontent.com/zewish/rmodal.js/master/dist/rmodal.min.js
 // @require          https://steamdb.info/static/js/tabbable.4f8f7fce.js
-// @connect          gist.githubusercontent.com
 // @grant            none
 // @run-at           document-end
 // ==/UserScript==
