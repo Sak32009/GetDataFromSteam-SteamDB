b442a0dabc3cd5ef1ea83d71931b1d8f20135c93
Sak32009 <Sak32009@users.noreply.github.com>
2019-03-18 12:14:46 +0100

    Test

---
+++ sak32009-get-dlc-info-from-steamdb-reworked.meta.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name             Get DLC Info from SteamDB Reworked
+// @namespace        sak32009-get-dlc-info-from-steamdb-reworked
+// @description      Get DLC Info from SteamDB Reworked
+// @author           Sak32009
+// @contributor      cs.rin.ru
+// @version          1.0.0
+// @license          MIT
+// @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/
+// @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
+// @updateURL        https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb-reworked.meta.js
+// @downloadURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb-reworked.user.js
+// @icon             https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-32.png
+// @icon64           https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-64.png
+// @resource         icon32 https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-32.png
+// @resource         icon64 https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-64.png
+// @match            *://steamdb.info/app/*
+// @match            *://steamdb.info/search/*
+// @require          https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require          https://raw.githubusercontent.com/zewish/rmodal.js/master/dist/rmodal.min.js
+// @require          https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
+// @require          https://steamdb.info/static/js/tabbable.4f8f7fce.js
+// @grant            GM_xmlhttpRequest
+// @grant            GM_getResourceURL
+// @grant            GM.xmlHttpRequest
+// @grant            GM.getResourceURL
+// @run-at           document-end
+// ==/UserScript==
