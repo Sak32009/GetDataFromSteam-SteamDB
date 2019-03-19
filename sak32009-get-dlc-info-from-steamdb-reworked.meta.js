993ca207f05c311dabb9057fbd5110be72885d9f
Sak32009 <Sak32009@users.noreply.github.com>
2019-03-19 08:56:11 +0100

    Update sak32009-get-dlc-info-from-steamdb-reworked.meta.js

--- sak32009-get-dlc-info-from-steamdb-reworked.meta.js
+++ sak32009-get-dlc-info-from-steamdb-reworked.meta.js
@@ -4,7 +4,7 @@
 // @description      Get DLC Info from SteamDB Reworked
 // @author           Sak32009
 // @contributor      cs.rin.ru
-// @version          1.0.0
+// @version          1.0.1
 // @license          MIT
 // @homepageURL      https://github.com/Sak32009/GetDLCInfoFromSteamDB/
 // @supportURL       http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
@@ -15,10 +15,9 @@
 // @resource         icon32 https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-32.png
 // @resource         icon64 https://raw.githubusercontent.com/Sak32009/GetDLCInfoFromSteamDB/master/sak32009-get-dlc-info-from-steamdb-64.png
 // @match            *://steamdb.info/app/*
-// @match            *://steamdb.info/search/*
 // @require          https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @require          https://raw.githubusercontent.com/zewish/rmodal.js/master/dist/rmodal.min.js
 // @require          https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
+// @require          https://raw.githubusercontent.com/zewish/rmodal.js/master/dist/rmodal.min.js
 // @require          https://steamdb.info/static/js/tabbable.4f8f7fce.js
 // @grant            GM_xmlhttpRequest
 // @grant            GM_getResourceURL
