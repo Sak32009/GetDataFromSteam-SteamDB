d390826e667fe63a6f9a51ba26da555b7a58a910
Sak32009 <Sak32009@users.noreply.github.com>
2019-08-11 18:16:59 +0200

    test

--- sak32009-get-dlc-info-from-steamdb.compatibility.js
+++ sak32009-get-dlc-info-from-steamdb.compatibility.js
@@ -8,87 +8,58 @@
 
 // REPLACE GM WITH SK
 this.SK = {
-	info: GM_info
+    info: GM_info
 };
 // IS TAMPERMONKEY?
-var isTampermonkey = GM_info.scriptHandler === "Tampermonkey";
+const isTampermonkey = GM_info.scriptHandler === "Tampermonkey";
 // IS GREASEMONKEY?
-var isGreasemonkey = GM_info.scriptHandler === "Greasemonkey";
+const isGreasemonkey = GM_info.scriptHandler === "Greasemonkey";
 // IS VIOLENTMONKEY?
-var isViolentmonkey = GM_info.scriptHandler === "Violentmonkey";
+const isViolentmonkey = GM_info.scriptHandler === "Violentmonkey";
 
 // TAMPERMONKEY || VIOLENTMONKEY
 if(isTampermonkey || isViolentmonkey){
-	// XML HTTP REQUEST
-	SK.xmlHttpRequest = GM_xmlhttpRequest;
-	// GET RESOURCE URL
-	SK.getResourceURL = GM_getResourceURL;
-	// GET RESOURCE TEXT
-	SK.getResourceText = function(id, rtn){
-		return rtn(GM_getResourceText(id));
-	};
-	// GET RESOURCE IMAGE // BASE64
-	SK.getResourceIMG = function(id, rtn){
-		return rtn(SK.getResourceURL(id));
-	};
-	// ADD STYLE
-	SK.addStyle = GM_addStyle;
+    // XML HTTP REQUEST
+    SK.xmlHttpRequest = GM_xmlhttpRequest;
+    // GET RESOURCE URL
+    SK.getResourceURL = GM_getResourceURL;
+    // GET RESOURCE TEXT
+    SK.getResourceText = (id, rtn) => rtn(GM_getResourceText(id));
+    // GET RESOURCE IMAGE // BASE64
+    SK.getResourceIMG = (id, rtn) => rtn(SK.getResourceURL(id));
+    // ADD STYLE
+    SK.addStyle = GM_addStyle;
 }
 
 // GREASEMONKEY
 if(isGreasemonkey){
-	// XML HTTP REQUEST
-	SK.xmlHttpRequest = GM.xmlHttpRequest;
-	// GET RESOURCE URL
-	SK.getResourceURL = GM.getResourceUrl;
-	// GET RESOURCE TEXT
-	SK.getResourceText = function(id, rtn){
-		(async function() {
-			var resourceURL = await SK.getResourceURL(id);
-			return SK.xmlHttpRequest({
-				method: "GET",
-				url: resourceURL,
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded"
-				},
-				onload: function(xhr) {
-					rtn(xhr.responseText);
-				}
-			});
-		})();
-	};
-	// ADD STYLE
-	SK.addStyle = function(css){
-		var head = document.getElementsByTagName("head")[0];
-		if (head.length) {
-			var style = document.createElement("style");
-			style.setAttribute("type", "text/css");
-			style.textContent = css;
-			head.appendChild(style);
-		}
-	};
+    // XML HTTP REQUEST
+    SK.xmlHttpRequest = GM.xmlHttpRequest;
+    // GET RESOURCE URL
+    SK.getResourceURL = GM.getResourceUrl;
+    // GET RESOURCE TEXT
+    SK.getResourceText = (id, rtn) => {
+        (async () => rtn(await SK.getResourceURL(id)))();
+    };
+    // ADD STYLE
+    SK.addStyle = url => {
+        const head = document.getElementsByTagName("head")[0];
+        if (head) {
+            const link = document.createElement("link");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("href", url);
+            link.setAttribute("crossorigin", "anonymous");
+            head.appendChild(link);
+            return link;
+        }
+        return null;
+    };
 }
 
-// VIOLENTMONKEY & GREASEMONKEY
+// VIOLENTMONKEY || GREASEMONKEY
 if(isViolentmonkey || isGreasemonkey){
-	// GET RESOURCE IMAGE // BASE64
-	SK.getResourceIMG = function(id, rtn){
-		(async function() {
-			var resourceURL = await SK.getResourceURL(id);
-			SK.xmlHttpRequest({
-				method: "GET",
-				url: resourceURL,
-				responseType: "blob",
-				onload: function(xhr){
-					var blob = xhr.response;
-					var reader = new FileReader();
-					reader.onload = function() {
-						var dataURL = reader.result;
-						rtn(dataURL);
-					};
-					reader.readAsDataURL(blob);
-				}
-			});
-		})();
-	};
+    // GET RESOURCE IMAGE // BASE64
+    SK.getResourceIMG = (id, rtn) => {
+        (async () => rtn(await SK.getResourceURL(id)))();
+    };
 }
