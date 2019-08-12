cd7c5c5c2cc6865d6c021087c7d4dfdf3fa8276b
Sak32009 <Sak32009@users.noreply.github.com>
2019-08-12 19:42:15 +0200

    test

--- sak32009-get-dlc-info-from-steamdb.compatibility.js
+++ sak32009-get-dlc-info-from-steamdb.compatibility.js
@@ -8,6 +8,7 @@
 
 // REPLACE GM WITH SK
 this.SK = {
+    // INFO
     info: GM_info
 };
 // IS TAMPERMONKEY?
@@ -58,8 +59,42 @@ if(isGreasemonkey){
 
 // VIOLENTMONKEY || GREASEMONKEY
 if(isViolentmonkey || isGreasemonkey){
+    // ADD META INFO
+    const metaNeed = ["author", "homepageURL", "supportURL"];
+    const metaStr = GM_info.scriptMetaStr.replace(/\r\n/g, "\n").split("\n");
+    metaStr.forEach(fullMeta => {
+        metaNeed.forEach(valMetaNeed => {
+            const startMeta = `// @${valMetaNeed} `;
+            if(fullMeta.startsWith(startMeta)){
+                const valueMetaNew = [];
+                const valueMetaSP = fullMeta.replace(startMeta, "").split(" ");
+                valueMetaSP.forEach(_value => {
+                    if(_value.length > 0 && _value !== " "){
+                        valueMetaNew.push(_value);
+                    }
+                });
+                SK.info.script[valMetaNeed] = valueMetaNew.join(" ");
+            }
+        });
+    });
     // GET RESOURCE IMAGE // BASE64
     SK.getResourceIMG = (id, rtn) => {
         (async () => rtn(await SK.getResourceURL(id)))();
     };
 }
+
+// TAMPERMONKEY, GREASEMONKEY AND VIOLENTMONKEY
+// LIST VALUES
+SK.listValues = window.localStorage;
+// SET VALUE
+SK.setValue = name => window.localStorage.setItem(`${SK.info.script.namespace}-${name}`);
+// GET VALUE
+SK.getValue = name => window.localStorage.getItem(`${SK.info.script.namespace}-${name}`);
+// DELETE VALUE
+SK.deleteValue = name => window.localStorage.removeItem(`${SK.info.script.namespace}-${name}`);
+// DELETE VALUES
+SK.deleteValues = () => window.localStorage.clear();
+// IS CHECKED VALUE
+SK.isCheckedValue = name => SK.getValue(name) === "true";
+// IS VALID VALUE
+SK.isValidValue = value => typeof value !== "undefined" && value !== null && value.length > 0;
