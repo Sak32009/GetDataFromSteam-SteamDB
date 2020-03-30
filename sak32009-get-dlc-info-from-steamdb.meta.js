1cec15a6999b8302f09d5a014bf0dda12cb667a7
Sak32009 <Sak32009@users.noreply.github.com>
2020-03-30 19:29:19 +0200

    Update sak32009-get-dlc-info-from-steamdb.meta.js

--- sak32009-get-dlc-info-from-steamdb.meta.js
+++ sak32009-get-dlc-info-from-steamdb.meta.js
@@ -4,7 +4,8 @@
 // @description   Get DLC Info from SteamDB
 // @author        Sak32009
 // @contributor   https://cs.rin.ru/forum/
-// @version       3.8.1
+// @year          2016 - 2020
+// @version       3.8.2
 // @license       MIT
 // @homepageURL   https://github.com/Sak32009/GetDLCInfoFromSteamDB/
 // @supportURL    http://cs.rin.ru/forum/viewtopic.php?f=10&t=71837
@@ -19,10 +20,10 @@
 // @require       https://cdnjs.cloudflare.com/ajax/libs/tabby/12.0.3/js/tabby.min.js
 // @require       https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb.compatibility.js
 
-// @resource      tabby     https://cdnjs.cloudflare.com/ajax/libs/tabby/12.0.3/css/tabby-ui.css
-// @resource      jModal    https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.2/jquery.modal.min.css
-// @resource      mainStyle https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb.css
-// @resource      icon      https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb-icon.png
+// @resource      tabby   https://cdnjs.cloudflare.com/ajax/libs/tabby/12.0.3/css/tabby-ui.min.css
+// @resource      jModal  https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.2/jquery.modal.min.css
+// @resource      main    https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb.css
+// @resource      icon    https://github.com/Sak32009/GetDLCInfoFromSteamDB/raw/master/sak32009-get-dlc-info-from-steamdb-icon.png
 
 // TamperMonkey & ViolentMonkey
 // @grant         GM_xmlhttpRequest
