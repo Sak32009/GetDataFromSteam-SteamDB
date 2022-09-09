35c8babd27bc85feb6c03eeb7966aa5f7cc79d89
Sak32009 <Sak32009@users.noreply.github.com>
2022-09-09 21:01:32 +0200

    v4.5.5

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,15 +5,20 @@ import { defineConfig } from 'vite';
 import viteMonkeyPlugin, { cdn as viteMonkeyCDNPlugin } from 'vite-plugin-monkey';
 import { name as pkgName, productName as pkgProductName } from '../package.json';
 
-export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
+export default defineConfig(() => {
   const rootPath = cwd();
   const viteRoot = join(rootPath, 'src');
   const viteEntry = join(viteRoot, 'main.ts');
   const viteOutDirectory = join(rootPath, 'dist');
-  const viteOutName = `${isDevelopment ? 'dev-' : ''}sak32009-get-data-from-steam-steamdb.user.js`;
+  const usFileName = 'sak32009-get-data-from-steam-steamdb.user.js';
+  const usFileMetaName = usFileName.replace('.user.js', '.meta.js');
   const viteConfig: UserConfigExport = {
     root: viteRoot,
+    resolve: {
+      alias: {
+        $: '../node_modules/vite-plugin-monkey/dist/client',
+      },
+    },
     plugins: [
       viteMonkeyPlugin({
         entry: viteEntry,
@@ -21,33 +26,28 @@ export default defineConfig(({ mode }) => {
           name: pkgProductName,
           namespace: pkgName,
           icon: 'https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/src/images/icon.png',
-          updateURL: `https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/${viteOutName}`,
-          downloadURL: `https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/${viteOutName}`,
+          updateURL: `https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/${usFileName}`,
+          downloadURL: `https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/${usFileMetaName}`,
           match: ['*://steamdb.info/app/*', '*://steamdb.info/depot/*', '*://store.steampowered.com/app/*'],
+          grant: ['unsafeWindow'],
+          require: ['https://cdnjs.cloudflare.com/ajax/libs/core-js/3.25.1/minified.min.js'],
         },
         build: {
-          fileName: viteOutName,
+          fileName: usFileName,
+          metaFileName: true,
           externalGlobals: {
             jquery: viteMonkeyCDNPlugin.cdnjs('$', 'jquery.min.js'),
             bootstrap: viteMonkeyCDNPlugin.cdnjs('bootstrap', 'js/bootstrap.bundle.min.js'),
             'crypto-js': viteMonkeyCDNPlugin.cdnjs('CryptoJS', 'crypto-js.min.js'),
           },
+          autoGrant: false,
         },
       }),
     ],
     build: {
       outDir: viteOutDirectory,
-      // emptyOutDir: true,
-      minify: true,
+      emptyOutDir: true,
     },
   };
-
-  if (isDevelopment) {
-    viteConfig.build = {
-      ...viteConfig.build,
-      minify: false,
-    };
-  }
-
   return viteConfig;
 });
