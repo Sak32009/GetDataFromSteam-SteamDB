181a32cdc9ee3329c59a17b1871d7346d4bd6a83
Sak32009 <Sak32009@users.noreply.github.com>
2023-01-07 19:02:52 +0100

    v4.6.2

--- dist/sak32009-get-data-from-steam-steamdb.meta.js
+++ dist/sak32009-get-data-from-steam-steamdb.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Get Data from Steam / SteamDB
 // @namespace     sak32009-gaxvyvrguokgtog
-// @version       4.6.1
+// @version       4.6.2
 // @author        Sak32009 (https://sak32009.github.io/)
 // @description   Get Data from Steam / SteamDB (ex Get DLC Info from SteamDB) is a userscript that extracts all data needed to generate DLCs formats and depot.sha1 for Steam games.
 // @license       MIT
@@ -9,8 +9,8 @@
 // @homepageURL   https://github.com/Sak32009/GetDLCInfoFromSteamDB/
 // @source        github:Sak32009/GetDLCInfoFromSteamDB
 // @supportURL    https://github.com/Sak32009/GetDLCInfoFromSteamDB/issues/
-// @downloadURL   https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/sak32009-get-data-from-steam-steamdb.meta.js
-// @updateURL     https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/sak32009-get-data-from-steam-steamdb.user.js
+// @downloadURL   https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/sak32009-get-data-from-steam-steamdb.user.js
+// @updateURL     https://raw.githack.com/Sak32009/GetDLCInfoFromSteamDB/master/dist/sak32009-get-data-from-steam-steamdb.meta.js
 // @match         *://steamdb.info/app/*
 // @match         *://steamdb.info/depot/*
 // @match         *://store.steampowered.com/app/*
@@ -18,9 +18,7 @@
 // @require       https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/sprintf/1.1.2/sprintf.min.js
-// @require       data:application/javascript,window.sprintfjs%3D%7Bsprintf%3Awindow.sprintf%7D%3B
 // @require       https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
-// @require       data:application/javascript,window.lodash%3Dwindow._%3B
 // @grant         unsafeWindow
-// @updatedAT     Tue, 03 Jan 2023 12:00:53 GMT
+// @updatedAT     Sat, 07 Jan 2023 17:59:42 GMT
 // ==/UserScript==
