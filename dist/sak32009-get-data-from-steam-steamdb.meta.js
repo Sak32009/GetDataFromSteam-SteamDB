64a3a7d0cf2716f09785883e0543c0ef5320104c
Sak32009 <Sak32009@users.noreply.github.com>
2023-07-26 22:39:27 +0200

    v5.0.9

--- dist/sak32009-get-data-from-steam-steamdb.meta.js
+++ dist/sak32009-get-data-from-steam-steamdb.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Get Data from Steam / SteamDB
 // @namespace      sak32009-gaxvyvrguokgtog
-// @version        5.0.8
+// @version        5.0.9
 // @author         Sak32009
 // @description    Get Data from Steam / SteamDB (ex Get DLC Info from SteamDB) it is a userscript that extracts all the information needed to generate the list of dlc and achievements in the chosen format, it generates the hashes list of the depots in sha1 to check the integrity and the appmanifest.acf of the Steam games.
 // @license        MIT
@@ -16,14 +16,14 @@
 // @match          *://store.steampowered.com/app/*
 // @require        https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js
 // @require        https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js
-// @require        https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js
+// @require        https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js
 // @require        https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
-// @require        https://cdn.jsdelivr.net/npm/jimp@0.22.8/browser/lib/jimp.min.js
+// @require        https://cdn.jsdelivr.net/npm/jimp@0.22.10/browser/lib/jimp.min.js
 // @require        https://cdn.jsdelivr.net/npm/fflate@0.8.0/umd/index.min.js
 // @require        https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
 // @connect        cdn.cloudflare.steamstatic.com
 // @grant          GM_xmlhttpRequest
 // @grant          unsafeWindow
 // @noframes
-// @updatedAT      Thu, 13 Jul 2023 16:25:45 GMT
+// @updatedAT      Wed, 26 Jul 2023 20:37:33 GMT
 // ==/UserScript==
